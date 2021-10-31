eeb1194c6cc8cc99c4f9d1af6f4806dddc715a5b
Scott Roberts <roberts.moto@gmail.com>
2021-10-31 13:18:19 +0100

    new feature counting a list is complete and tested with '#' will be ignored, and an empty message aborts the commit.

--- src/some_program.py
+++ src/some_program.py
@@ -4,3 +4,14 @@
 my_str_var = ""
 new_var_feature = ""
 another_var_feature = ""
+
+def some_function(mylist=[]):
+    _mylist = mylist
+    for x in _mylist:
+        print(f"x is {x}")
+
+mylist = ['one', 'two', 'three', 'four']
+
+if __name__ == "__main__":
+    some_function(mylist=mylist)
+
